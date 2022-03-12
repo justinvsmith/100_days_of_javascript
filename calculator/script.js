bd7c206ca488930dc04e99e6c97d7ebd2e20ba44
justinvsmith <smith.justinv@gmail.com>
2022-03-11 23:49:02 -0800

    Adding calculator project

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+const keys = document.getElementsByTagName("li"),
+    result = document.querySelector("p"),
+    clear = document.querySelector(".clear");
+
+
+for(let i=0; i<keys.length; i++) {
+    if(keys[i].innerHTML === "=") {
+        keys[i].addEventListener("click", calculate);
+    } else {
+        keys[i].addEventListener("click", addToCurrentValue(i));
+    }
+}
+
+function addToCurrentValue(i) {
+    return () => {
+        if(keys[i].innerHTML === "÷") {
+            result.innerHTML += "/ ";
+        } else if(keys[i].innerHTML === "x") {
+            result.innerHTML += "*";
+        } else {
+            result.innerHTML += keys[i].innerHTML;
+        }
+    }
+}
+
+function calculate() {
+    return (result.innerHTML = eval(result.innerHTML));
+}
+
+clear.addEventListener("click", () => {
+    result.innerHTML = "";
+})
