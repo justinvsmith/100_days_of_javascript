439351d1dd66639a0ac0476f4f194bdd907a9592
justinvsmith <smith.justinv@gmail.com>
2022-03-17 22:37:29 -0700

    updated the accordionMenu project

--- accordionMenu/script.js
+++ accordionMenu/script.js
@@ -2,12 +2,26 @@ const accordion = document.querySelectorAll(".accordion");
 
 for(let i=0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+        // this.classList.toggle("active");
         const desc = this.nextElementSibling;
+        const allDesc = document.querySelectorAll(".desc");
+        const activeAcc = document.getElementsByClassName("accordion active");
         if(desc.style.maxHeight) {
             desc.style.maxHeight = null;
+            this.classList.remove("active");
         } else {
+            
+            for(let i = 0; i < activeAcc.length; i++) {
+                activeAcc[i].classList.remove("active");
+            }
+
+            for(let i = 0; i<allDesc.length; i++) {
+                allDesc[i].style.maxHeight = null;
+            }
+
             desc.style.maxHeight = desc.scrollHeight + "px";
+            this.classList.add("active");
+
         }
     })
 }
