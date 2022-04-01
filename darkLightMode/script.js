7b4ce3b1de9a40c1517cb4c85b30c92deeed112f
justinvsmith <smith.justinv@gmail.com>
2022-04-01 14:32:14 -0700

    Added darkLightMode and floatingActionButton projects

---
+++ darkLightMode/script.js
@@ -0,0 +1,33 @@
+const toggleDarkMode = document.querySelector(".toggle-darkmode"),
+    toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    toggleText.textContent = "Light";
+    localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    toggleText.textContent = "Dark";
+    localStorage.setItem("darkMode", null);
+}
+
+// Save DarkMode History
+if(darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+// Add Event Listener
+toggleDarkMode.addEventListener("click", () => {
+    let darkMode = localStorage.getItem("darkMode");
+
+    if(darkMode !== "enabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
